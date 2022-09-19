1ba9f924ad449a3f14cc4d90c90ff39cd584e061
Jaejin-Sung <brokearphone@yonsei.ac.kr>
2022-09-19 19:45:59 +0900

    두번째 커밋

--- src/sgdfsdf/practice/HelloJava.java
+++ src/sgdfsdf/practice/HelloJava.java
@@ -4,7 +4,8 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		HelloJava.say("Hi~");
+//		HelloJava.say("Hi~");
+		HelloJava.say("Hello");
 	}
 	public static void say(String msg) {
 		// TODO Auto-generated method stub
